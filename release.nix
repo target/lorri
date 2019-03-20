2fd845efc7ed6f3f85d669c221d044981acb0af9
Graham Christensen <graham@grahamc.com>
2019-03-20 14:45:39 -0400

    Changelog entry for the updated watch behavior

--- release.nix
+++ release.nix
@@ -3,7 +3,17 @@
   package = import ./default.nix { inherit src; };
 
   changelog = {
+    # Find the current version number with `git log --prety=%h | wc -l`
     entries = [
+      {
+        version = 129;
+        changes = ''
+          `lorri watch` now supports Vim's method of writing to files.
+
+          Previously, the watch behavior would support a maximum of
+          three reloads (#66).
+        '';
+      }
       {
         version = 59;
         changes = ''
