ee004d8fdf174597256e7c1b99ea521c93243ed8
Graham Christensen <graham@grahamc.com>
2019-04-02 20:08:22 -0400

    travis-ci: init

---
+++ .travis.yml.nix
@@ -0,0 +1,55 @@
+let
+  pkgs = import <nixpkgs> {};
+
+  hosts = {
+    linux = {
+      os = "linux";
+      language = "nix";
+      nix = "2.2.1";
+    };
+
+    macos = {
+      os = "osx";
+      language = "nix";
+      nix = "2.0";
+    };
+  };
+
+  scripts = {
+    builds = {
+      name = "nix-build";
+      script = ''
+        nix-build
+        nix-env -i ./result
+        lorri self-upgrade local $(pwd)
+      '';
+    };
+
+    lints = {
+      name = "cargo build & linters";
+      script = ''
+        nix-shell --run ci_check
+        cat $(nix-build ./.travis.yml.nix --no-out-link) > .travis.yml
+        git diff -q ./.travis.yml
+      '';
+    };
+  };
+
+  jobs = {
+    languge = "nix";
+    matrix.include = [
+      (hosts.linux // scripts.lints)
+      (hosts.macos // scripts.lints)
+
+      (hosts.linux // scripts.builds)
+      (hosts.macos // scripts.builds)
+    ];
+  };
+in pkgs.runCommand "travis.yml" {
+  buildInputs = [ pkgs.remarshal ];
+  passAsFile = [ "jobs" ];
+  jobs = builtins.toJSON jobs;
+}
+''
+  remarshal -if json -i $jobsPath -of yaml -o $out
+''
