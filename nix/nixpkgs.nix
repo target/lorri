0b1d6863a25a7bfe46561f47aa48311e9f56f087
Philip Patsch <philip.patsch@tweag.io>
2019-04-18 18:22:03 +0200

    nix/nixpkgs.nix: add buildSandbox and mdsh
    buildSandbox is a helper function to create wrappers that execute a
    given derivation’s binaries in a lightweight sandbox, optionally with
    runtime paths bind-mounted.
    
    mdsh is a tool to execute commands in markdown files, which we are
    going to use to put our tutorial and README in CI.

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -6,6 +6,13 @@ let
     sha256 = srcDef.sha256;
   };
 
+  # provides buildSandbox
+  vuizvui = import (builtins.fetchTarball {
+    # 2019-09-12, buildSandbox patches
+    url = "https://github.com/openlab-aux/vuizvui/archive/6f0954ba13c93ec8fb34cce104a81d291f41555f.tar.gz";
+
+    sha256 = "13ad3ccspj7gpgx0qvkppbnpp7sl2bqa8i86bsi2ysrdjc36km9n";
+  }) {};
 
   # The Mozilla overlay exposes dynamic, constantly updating
   # rust binaries for development tooling. Not recommended
@@ -23,6 +30,31 @@ let
     builtins.fetchTarball
     https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz
   );
+
+  our-overlay = self: super: {
+
+    mdsh = super.rustPlatform.buildRustPackage rec {
+      name = "mdsh-${version}";
+      version = "unreleased";
+
+      src = super.fetchFromGitHub {
+        owner = "zimbatm";
+        repo = "mdsh";
+        # 2018-04-18, fail on failing commands and show stderr
+        rev = "0650c21f833deb8993007e285d6219fd2279d58d";
+        sha256 = "1rjfik9rxksydgqjh5g9irz75x7jy00v23d8by4jgdi16xjcbbsy";
+      };
+
+
+      cargoSha256 = "11kzl0ns84xhdacn0k7nilgzgpwazmaaqdjf2kcarxf2h01b0rjv";
+    };
+
+    buildSandbox = vuizvui.pkgs.buildSandbox;
+
+  };
+
 in import nixpkgs {
-  overlays = if enableMozillaOverlay then [ mozilla-overlay ] else [];
+  overlays =
+    [ our-overlay ]
+    ++ (if enableMozillaOverlay then [ mozilla-overlay ] else []);
 }
