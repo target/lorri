5f8dd2db08bab6b7bdae59401ada7ec3634567ea
tobias pflug <tobias.pflug@gmail.com>
2019-05-17 19:53:35 +0200

    Print custom message per envrc/shell.nix

--- src/ops/init/mod.rs
+++ src/ops/init/mod.rs
@@ -6,9 +6,9 @@ use std::io;
 use std::io::Write;
 use std::path::Path;
 
-fn create_if_missing(path: &Path, contents: &str) -> Result<(), io::Error> {
+fn create_if_missing(path: &Path, contents: &str, msg: &str) -> Result<(), io::Error> {
     if path.exists() {
-        println!("- Skipping existing {}", path.display());
+        println!("- {} {}", msg, path.display());
         Ok(())
     } else {
         let mut f = File::create(path)?;
@@ -28,8 +28,17 @@ fn to_op(e: Result<(), io::Error>) -> OpResult {
 /// See the documentation for lorri::cli::Command::Init for
 /// more details
 pub fn main(default_shell: &str, default_envrc: &str) -> OpResult {
-    to_op(create_if_missing(Path::new("./shell.nix"), default_shell))?;
-    to_op(create_if_missing(Path::new("./.envrc"), default_envrc))?;
+    to_op(create_if_missing(
+        Path::new("./shell.nix"),
+        default_shell,
+        "shell.nix exists, skipping. Make sure it is of a form that works with nix-shell.",
+    ))?;
+
+    to_op(create_if_missing(
+        Path::new("./.envrc"), 
+        default_envrc,
+        ".envrc exists, skipping. Please add 'eval \"$(lorri direnv)\" to it to set up lorri support.",
+    ))?;
 
     ok_msg(String::from("\nSetup done."))
 }
