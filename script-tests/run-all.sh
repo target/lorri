403d72c04883ae8628eb5210011af403d5fb42fd
Graham Christensen <graham@grahamc.com>
2019-06-05 10:58:10 -0400

    envrc script tests: delete

--- script-tests/run-all.sh
+++ script-tests/run-all.sh
@@ -6,9 +6,6 @@ shellcheck "$0"
 
 cd "$(dirname "$0")"
 
-shellcheck ./envrc.sh
 shellcheck ../nix/bogus-nixpkgs/builder.sh
 shellcheck ../src/ops/prompt.sh
 shellcheck ../src/ops/direnv/envrc.bash
-
-./envrc.sh
