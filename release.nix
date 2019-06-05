00e5cb1516f934c64ac7cd3fc9305706b71cefe7
Graham Christensen <graham@grahamc.com>
2019-06-05 16:48:18 -0400

    changelog: update with shellhook support

--- release.nix
+++ release.nix
@@ -6,6 +6,41 @@
     # Find the current version number with `git log --prety=%h | wc -l`
     entries = [
       {
+        version = 130;
+        changes = ''
+          `lorri watch` now supports executing shellHooks.
+
+          - shellHooks run inside `lorri watch`, and not in `direnv`
+
+            This means they will execute only once, while inside the
+            build sandbox.
+
+            shellHooks are not to be used for starting services or
+            printing text to the CLI, as these actions will not
+            execute when the shell is entered.
+
+          - Environment variables which are appended to the
+            environment with Nixpkgs'
+            addToSearchPathWithCustomDelimiter function will
+            automatically be appended to the user's environment when
+            entering the lorri shell.
+
+            Notably, this means Go support.
+
+            Many functions in Nixpkgs use
+            addtoSearchPathWithCustomDelimiter, including:
+
+             - addToSearchPath
+             - addPythonPath
+             - R libraries
+
+            among others.
+
+            Overall, this should allow a much more "nix-shell"-like
+            experience.
+        '';
+      }
+      {
         version = 129;
         changes = ''
           `lorri watch` now supports Vim's method of writing to files.
