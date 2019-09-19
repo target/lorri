4872ea4a9621c13ab612db4efc2a94512f9260b3
Philip Patsch <philip.patsch@tweag.io>
2019-09-19 13:08:49 +0200

    shell.nix: move mdsh-sandbox into nix/ci.nix

--- shell.nix
+++ shell.nix
@@ -15,49 +15,9 @@ let
         stableVersion = "1.35.0";
       });
 
-  # command to run mdsh inside of a lightweight sandbox
-  # with a tmpfs root filesystem (that is deleted after execution).
-  # You can run it like this, without having to worry about any dangerous
-  # commands being executed on your files:
-  # $ env lorri-mdsh-sandbox -i $(realpath ./README.md)
-  mdsh-sandbox =
-    let
-      emptySetupScript = pkgs.writeShellScriptBin "lorri-mdsh-sandbox" "";
-      setupScript = pkgs.writeShellScriptBin "lorri-mdsh-sandbox" ''
-        set -e
-        export HOME=/work/sandbox-home
-        mkdir -p "$HOME"
 
-        WORK_DIR=/work/lorri
-        # copy the lorri repo to the temporary sandbox work directory
-        cp -r "${LORRI_ROOT}" "$WORK_DIR"
+  ci = import ./nix/ci.nix { inherit pkgs LORRI_ROOT; rust = rustChannels.stable.rust; };
 
-        # required to run `nix-env` in mdsh
-        mkdir /work/sandbox-home/.nix-defexpr
-
-        # clean env and run mdsh with extra arguments
-        env -i \
-          USER="$USER" \
-          HOME="$HOME" \
-          PATH="$PATH" \
-          NIX_PROFILE="$HOME/nix-profile" \
-            ${pkgs.mdsh}/bin/mdsh \
-              --work_dir "$WORK_DIR/$SUBDIR" \
-              "$@"
-      '';
-    in
-      # on Darwin there’s no way to sandbox, so the script should be a no-op
-      if pkgs.stdenv.isDarwin then emptySetupScript
-
-      else pkgs.buildSandbox setupScript {
-        # the whole nix store is mounted in the sandbox,
-        # to make nix builds possible
-        fullNixStore = true;
-        # The path in "$LORRI_ROOT" is magically mounted into the sandbox
-        # read-write before running `setupScript`, at exactly the same
-        # absolute path as outside of the sandbox.
-        paths.required = [ LORRI_ROOT ];
-      };
 
 in
 pkgs.mkShell rec {
@@ -78,7 +38,7 @@ pkgs.mkShell rec {
     pkgs.nix
 
     # for auto-checking the README.md and tutorial
-    mdsh-sandbox
+    ci.mdsh-sandbox
   ] ++
   pkgs.stdenv.lib.optionals pkgs.stdenv.isDarwin [
     pkgs.darwin.Security
