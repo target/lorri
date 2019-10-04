d426762c37237b7e20014cb4b5ca7c2700a3406e
Philip Patsch <philip.patsch@tweag.io>
2019-10-04 16:08:15 +0200

    ops/watch: move once and forever cases into subroutines

--- src/ops/watch.rs
+++ src/ops/watch.rs
@@ -9,46 +9,53 @@ use std::io::Write;
 use std::sync::mpsc::channel;
 use std::thread;
 
+/// See the documentation for lorri::cli::Command::Shell for more
+/// details.
+pub fn main(project: Project, opts: WatchOptions) -> OpResult {
+    if opts.once {
+        main_run_once(project)
+    } else {
+        main_run_forever(project)
+    }
+}
+
+fn main_run_once(project: Project) -> OpResult {
+    let mut build_loop = BuildLoop::new(&project);
+    match build_loop.once() {
+        Ok(msg) => {
+            print_build_message(msg);
+            ok()
+        }
+        Err(BuildError::Unrecoverable(err)) => Err(ExitError::err(100, format!("{:?}", err))),
+        Err(BuildError::Recoverable(exit_failure)) => {
+            Err(ExitError::errmsg(format!("{:#?}", exit_failure)))
+        }
+    }
+}
+
+fn main_run_forever(project: Project) -> OpResult {
+    let (tx, rx) = channel();
+    let build_thread = {
+        thread::spawn(move || {
+            let mut build_loop = BuildLoop::new(&project);
+            build_loop.forever(tx);
+        })
+    };
+
+    for msg in rx {
+        print_build_message(msg);
+        let _ = std::io::stdout().flush();
+    }
+
+    build_thread.join().unwrap();
+
+    ok()
+}
+
 // TODO: this should probably be something more structured
 fn print_build_message<A>(msg: A)
 where
     A: Debug,
 {
     println!("{:#?}", msg);
-}
-
-/// See the documentation for lorri::cli::Command::Shell for more
-/// details.
-pub fn main(project: Project, opts: WatchOptions) -> OpResult {
-    let (tx, rx) = channel();
-
-    if opts.once {
-        let mut build_loop = BuildLoop::new(&project);
-        match build_loop.once() {
-            Ok(msg) => {
-                print_build_message(msg);
-                ok()
-            }
-            Err(BuildError::Unrecoverable(err)) => Err(ExitError::err(100, format!("{:?}", err))),
-            Err(BuildError::Recoverable(exit_failure)) => {
-                Err(ExitError::errmsg(format!("{:#?}", exit_failure)))
-            }
-        }
-    } else {
-        let build_thread = {
-            thread::spawn(move || {
-                let mut build_loop = BuildLoop::new(&project);
-                build_loop.forever(tx);
-            })
-        };
-
-        for msg in rx {
-            print_build_message(msg);
-            let _ = std::io::stdout().flush();
-        }
-
-        build_thread.join().unwrap();
-
-        ok()
-    }
 }
