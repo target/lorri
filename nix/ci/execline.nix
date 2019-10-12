42c0b9dd5d8469de8dec2b25fb0e3566f0dc8218
Philip Patsch <philip.patsch@tweag.io>
2019-10-12 03:17:49 +0200

    nix/ci/execline: use `-W` instead of `EXECLINE_STRICT`
    Using `-W` actually exits if the wrong amount of arguments is given.

--- nix/ci/execline.nix
+++ nix/ci/execline.nix
@@ -71,8 +71,7 @@ let
   # Everything is escaped correctly.
   # TODO upstream into nixpkgs
   writeExeclineCommon = writer: name: { readNArgs ? 0 }: argList: writer name ''
-    #!${execlineb-with-builtins}/bin/execlineb -s${toString readNArgs}
-    export EXECLINE_STRICT 2
+    #!${execlineb-with-builtins}/bin/execlineb -Ws${toString readNArgs}
     ${escapeExecline argList}
   '';
   writeExecline = writeExeclineCommon pkgs.writeScript;
