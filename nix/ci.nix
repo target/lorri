2f11285db3e4430c4debfa62869db09ed5a46492
Philip Patsch <philip.patsch@tweag.io>
2019-11-08 18:29:11 +0100

    nix/ci: add pathAdd

--- nix/ci.nix
+++ nix/ci.nix
@@ -11,6 +11,24 @@ let
     (writeExecline name {})
   ];
 
+  # Takes a `mode` string and produces a script,
+  # which modifies PATH given by $1 and execs into the rest of argv.
+  # `mode`s:
+  #   "set": overwrite PATH, set it to $1
+  #   "append": append the given $1 to PATH
+  #   "prepend": prepend the given $1 to PATH
+  pathAdd = mode:
+    let exec = [ "exec" "$@" ];
+        importPath = [ "importas" "PATH" "PATH" ];
+        set = [ "export" "PATH" "$1" ] ++ exec;
+        append = importPath ++ [ "export" "PATH" ''''${PATH}:''${1}'' ] ++ exec;
+        prepend = importPath ++ [ "export" "PATH" ''''${1}:''${PATH}'' ] ++ exec;
+    in writeExecline "PATH_${mode}" { readNArgs = 1; }
+        (if    mode == "set"     then set
+        else if mode == "append" then append
+        else if mode == "prepend" then prepend
+        else abort "don’t know mode ${mode}");
+
   # shellcheck file
   shellcheck = file: writeExecline "lint-shellcheck" {} [
     "cd" LORRI_ROOT
