5a98326c6fc4e040437cc1cc2213538992716efb
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-01-20 09:43:30 +0100

    services integration test: add logging

--- tests/services/main.rs
+++ tests/services/main.rs
@@ -1,8 +1,13 @@
+use lorri::cli::Command;
+use lorri::logging;
 use lorri::ops::services;
 use std::time::Instant;
 
 #[test]
 pub fn service_starts() -> std::io::Result<()> {
+    let log = logging::root(1, &Command::Daemon);
+    let _guard = slog_scope::set_global_logger(log);
+
     let tempdir = tempfile::tempdir()?;
     let services_nix = tempdir.as_ref().join("services.nix");
     let file_to_touch = tempdir.as_ref().join("touchit");
@@ -28,5 +33,8 @@ pub fn service_starts() -> std::io::Result<()> {
     }
 
     assert!(file_touched, "service did not run successfully");
+
+    drop(tempdir);
+    drop(_guard);
     Ok(())
 }
