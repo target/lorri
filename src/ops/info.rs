8deb36ef57ae5f2f9ad5230ae00b38ee1576ea6a
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-02-04 15:04:04 +0100

    lorri info: show MAJOR.MINOR version

--- src/ops/info.rs
+++ src/ops/info.rs
@@ -7,7 +7,7 @@ use crate::project::{roots::Roots, Project};
 /// See the documentation for lorri::cli::Command::Info for more
 /// details.
 pub fn main(project: Project) -> OpResult {
-    println!("lorri version: {}", crate::VERSION_BUILD_REV);
+    println!("lorri version: {}", crate::LORRI_VERSION);
     let root_paths = Roots::from_project(&project).paths();
     let OutputPaths { shell_gc_root } = &root_paths;
     if root_paths.all_exist() {
