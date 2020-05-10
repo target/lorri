d3ab9206f95c88ac692324cf8554477d28337fb6
Cole Helbling <cole.e.helbling@outlook.com>
2020-05-09 23:49:13 -0700

    nix/: update dependencies
    NixOS 20.03 has been released. Thus, we can update to the latest nixpkgs
    and drop the `nixpkgs-fmt` overlay.

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -4,11 +4,5 @@ let
     url = "https://github.com/NixOS/nixpkgs/archive/${srcDef.rev}.tar.gz";
     sha256 = srcDef.sha256;
   };
-  # The version of nixpkgs-fmt in release-19.09 [1] does not support --check, which
-  # we need for CI. Hence this overlay.
-  # [1] https://github.com/NixOS/nixpkgs/blob/release-19.09/pkgs/tools/nix/nixpkgs-fmt/default.nix
-  nixpkgs-fmt-overlay = self: super: {
-    nixpkgs-fmt = self.callPackage ./nixpkgs-fmt.nix {};
-  };
 in
-import nixpkgs { overlays = [ nixpkgs-fmt-overlay ]; }
+import nixpkgs {}
