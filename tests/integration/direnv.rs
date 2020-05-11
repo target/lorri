e0457002ebd62ac785e4d4c22a37dd22acc125df
Cole Helbling <cole.e.helbling@outlook.com>
2020-05-11 08:32:25 -0700

    tests: add simple test for IN_LORRI_SHELL

--- tests/integration/direnv.rs
+++ tests/integration/direnv.rs
@@ -1,1 +1,19 @@
+use std::iter::FromIterator;
+use std::path::PathBuf;
 
+use crate::direnvtestcase::{DirenvTestCase, DirenvValue};
+
+#[test]
+fn in_lorri_shell() {
+    let mut testcase = DirenvTestCase::new("basic");
+    testcase.evaluate().expect("Failed to build the first time");
+
+    let env = testcase.get_direnv_variables();
+    let shell = PathBuf::from_iter(&[env!("CARGO_MANIFEST_DIR"), "tests", "integration", "basic"])
+        .join("shell.nix");
+
+    assert_eq!(
+        env.get_env("IN_LORRI_SHELL"),
+        DirenvValue::Value(shell.to_str().unwrap())
+    );
+}
