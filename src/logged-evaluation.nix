a34d28807409e4360ca37f2a3096a38d2d2918ff
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-05-15 09:05:26 +0200

    Revert "Remove "or"s from logged-evaluation.nix"
    This reverts commit 13a06d302131fc8348a735b10e9febb7f9d6a9b7.

--- src/logged-evaluation.nix
+++ src/logged-evaluation.nix
@@ -40,29 +40,29 @@ let
   # references to all the store paths needed, preventing the shell's
   # actual environment from being deleted.
   wrapped-project = shell: services: derivation (
-    shell.drvAttrs // {
-      name = "lorri-wrapped-project-${shell.name}";
+    (shell.drvAttrs or {}) // {
+      name = "lorri-wrapped-project-${shell.name or "unknown"}";
 
-      origExtraClosure = shell.extraClosure;
+      origExtraClosure = shell.extraClosure or [];
       extraClosure = runtimeCfg.closure;
 
-      origBuilder = shell.builder;
+      origBuilder = shell.builder or null;
       builder = runtimeCfg.builder;
 
-      origSystem = shell.system;
+      origSystem = shell.system or null;
       system = builtins.currentSystem;
 
-      origPATH = shell.PATH;
+      origPATH = shell.PATH or "";
       PATH = runtimeCfg.path;
 
       # The derivation we're examining may be multi-output. However,
       # this builder only produces the «out» output. Not specifying a
       # single output means we would fail to start a shell for those
       # projects.
-      origOutputs = shell.outputs;
+      origOutputs = shell.outputs or [];
       outputs = [ "out" ];
 
-      origPreHook = shell.preHook;
+      origPreHook = shell.preHook or "";
       preHook = ''
         # Redefine addToSearchPathWithCustomDelimiter to integrate with
         # lorri's environment variable setup map. Then, call the original
@@ -116,10 +116,10 @@ let
           }
         fi
 
-        ${shell.preHook}
+        ${shell.preHook or ""}
       '';
 
-      origArgs = shell.args;
+      origArgs = shell.args or [];
       args = [
         "-e"
         (
@@ -155,17 +155,7 @@ let
     }
   );
 
-  shell = {
-    name = "unknown";
-    args = [];
-    builder = null;
-    drvAttrs = {};
-    extraClosure = [];
-    outputs = [];
-    preHook = "";
-    system = null;
-    PATH = "";
-  } // (if shellSrc == null then {} else logged shellSrc);
+  shell = if shellSrc == null then {} else logged shellSrc;
   services = if servicesSrc == null then [] else logged servicesSrc;
 in
 wrapped-project shell services
