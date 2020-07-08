94c549490b39669412c3e0e146cf19d8c44cd744
Judson Lester <nyarly@gmail.com>
2020-07-08 13:40:29 -0700

    chore(ops/ping): More readable optional handling

--- src/ops/ping.rs
+++ src/ops/ping.rs
@@ -1,17 +1,16 @@
 //! Run a BuildLoop for `shell.nix`, watching for input file changes.
 //! Can be used together with `direnv`.
 use crate::internal_proto;
-use crate::ops::error::{ok, ExitError, OpResult};
+use crate::ops::error::{ok, OpResult};
 use crate::NixFile;
 use std::convert::TryFrom;
 
 /// See the documentation for lorri::cli::Command::Ping_ for details.
 pub fn main(nix_file: NixFile, addr: Option<String>) -> OpResult {
-    let address = addr
-        .ok_or(Err(()))
-        .or_else::<ExitError, _>(|_: Result<String, _>| {
-            Ok(crate::ops::get_paths()?.daemon_socket_address())
-        })?;
+    let address = match addr {
+        Some(a) => a,
+        None => crate::ops::get_paths()?.daemon_socket_address()
+    };
     let shell_nix = internal_proto::ShellNix::try_from(&nix_file).unwrap();
 
     use internal_proto::VarlinkClientInterface;
